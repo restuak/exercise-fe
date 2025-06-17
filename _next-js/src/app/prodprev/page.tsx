aac328357adb3300daa6c46abc54a7a591572ddd
restuak <restuaka.works@gmail.com>
2025-06-17 22:49:18 +0700

    new-update

---
+++ _next-js/src/app/prodprev/page.tsx
@@ -0,0 +1,47 @@
+import styles from "./page.module.css";
+
+export default function BlogPrev() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.container}>
+        <div className={styles.blogImage}>
+          <img src="/bp-content.png" alt="Blog Image" />
+        </div>
+        <div className={styles.blogImage2}>
+          <img src="/bp-content2.png" alt="Blog Image" />
+        </div>
+        <div className={styles.content}>
+          <div className={styles.contentTag}>
+            <div className={styles.contentTagText}>
+              <p>Learning</p>
+            </div>
+          </div>
+
+          <div className={styles.contentDate}>
+            <p>Published 21 Dec 2023</p>
+          </div>
+
+          <div className={styles.contentTitle}>
+            <p>HTML & CSS foundations</p>
+          </div>
+
+          <div className={styles.contentDesc}>
+            <p>
+              These languages are the backbone of every website, defining
+              structure, content, and presentation.
+            </p>
+          </div>
+
+          <div className={styles.author}>
+            <div className={styles.authorImage}>
+              <img src="/bp-profile.png" alt="Author Image" />
+            </div>
+            <div className={styles.authorName}>
+              <p>Greg Hooper</p>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
