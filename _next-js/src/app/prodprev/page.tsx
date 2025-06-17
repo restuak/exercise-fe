069c33a146d04a2c5b31e29a78b764bf33e392e7
restuak <restuaka.works@gmail.com>
2025-06-18 00:00:21 +0700

    update-exercise

--- _next-js/src/app/prodprev/page.tsx
+++ _next-js/src/app/prodprev/page.tsx
@@ -2,8 +2,8 @@ import styles from "./page.module.css";
 
 export default function ProdPrev() {
   return (
-   <div className={styles.page}>
-    <main className={styles.container}>
+    <div className={styles.page}>
+      <main className={styles.container}>
         <div className={styles.image}>
           <img src="/pp-content.png" alt="Parfume Image" />
         </div>
@@ -12,33 +12,32 @@ export default function ProdPrev() {
         </div>
 
         <div className={styles.content}>
-            <div className={styles.category}>
-              <p>PERFUME</p>
-            </div>
+          <div className={styles.category}>
+            <p>PERFUME</p>
+          </div>
 
-            <div className={styles.title}>
-              <p>Gabrielle Essence Eau De Parfum</p>
-            </div>
+          <div className={styles.title}>
+            <p>Gabrielle Essence Eau De Parfum</p>
+          </div>
 
-            <div className={styles.desc}>
-              <p>
-                A floral, solar and voluptuous interpretation composed by Olivier
-                Polge, Perfumer-Creator for the House of CHANEL.
-              </p>
-            </div>
+          <div className={styles.desc}>
+            <p>
+              A floral, solar and voluptuous interpretation composed by Olivier
+              Polge, Perfumer-Creator for the House of CHANEL.
+            </p>
+          </div>
 
-            <div className={styles.price}>
-              <div className={styles.priceAft}>$149.99</div>
-              <div className={styles.priceBef}>$169.99</div>
-            </div>
+          <div className={styles.price}>
+            <div className={styles.priceAft}>$149.99</div>
+            <div className={styles.priceBef}>$169.99</div>
+          </div>
 
-            <div className={styles.cart}>
-              <img src="/pp-chart.png" alt="Chart" />
-              <div className={styles.cartText}>Adds to Cart</div>
-            </div>
+          <div className={styles.cart}>
+            <img src="/pp-chart.png" alt="Chart" />
+            <div className={styles.cartText}>Adds to Cart</div>
+          </div>
         </div>
-    </main>
-  </div>
-
+      </main>
+    </div>
   );
 }
