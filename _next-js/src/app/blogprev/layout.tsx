ec965ae5837ad010b8b9e2a78d1c11e1d05e3bc1
restuak <restuaka.works@gmail.com>
2025-06-18 00:18:34 +0700

    update exercise

---
+++ _next-js/src/app/blogprev/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "Blog Preview",
+  description: "Exercise: Blog Preview",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
---
+++ _next-js/src/app/prodprev/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "Product Preview",
+  description: "Exercise: Product Preview",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
